eac7bffc32ab09358c8160214eb9388c42856bdd
Minhyuk Kim <rhrlwld@gmail.com>
2022-08-14 23:23:09 +0900

    add update function

--- src/main/java/com/SpringAWS/study/web/PostsApiController.java
+++ src/main/java/com/SpringAWS/study/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.SpringAWS.study.web;
 
 import com.SpringAWS.study.service.posts.PostsService;
+import com.SpringAWS.study.web.dto.PostsResponseDto;
 import com.SpringAWS.study.web.dto.PostsSaveRequestDto;
+import com.SpringAWS.study.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,4 +17,19 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 }
+
+/**
+  @PathVariable: 경로에 변수를 넣을 수 있도록 해줌!
+  @g
+ */
---
+++ src/main/java/com/SpringAWS/study/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.SpringAWS.study.web.dto;
+
+import com.SpringAWS.study.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
