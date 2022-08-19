56354e076832b149a92a81255df04234dda6c2ab
Minhyuk Kim <rhrlwld@gmail.com>
2022-08-20 02:06:12 +0900

    add main view

---
+++ src/test/java/com/SpringAWS/study/web/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package com.SpringAWS.study.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+
+}
+
+/**
+ * testRestTemplate.getForObject(): 기본 http 헤더를 사용하 결과를 객체로 반환한다.
+ *
+ */
