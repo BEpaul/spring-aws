13b6acd9cf074609341a566fbe23b4219a40383b
Minhyuk Kim <rhrlwld@gmail.com>
2022-08-21 21:32:59 +0900

    add save, read view

--- src/main/java/com/SpringAWS/study/service/posts/PostsService.java
+++ src/main/java/com/SpringAWS/study/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.SpringAWS.study.service.posts;
 
 import com.SpringAWS.study.domain.posts.Posts;
 import com.SpringAWS.study.domain.posts.PostsRepository;
+import com.SpringAWS.study.web.dto.PostsListResponseDto;
 import com.SpringAWS.study.web.dto.PostsResponseDto;
 import com.SpringAWS.study.web.dto.PostsSaveRequestDto;
 import com.SpringAWS.study.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // == .map(posts -> new PostsListsResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
